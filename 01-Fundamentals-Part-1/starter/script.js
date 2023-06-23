c5d6d249a3b9183fca095125d70b1346d7a1f218
ZHDreamer <ZHDreamerqaq@gmail.com>
2023-06-23 18:10:45 +0800

    complete code challenge 2

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,6 @@
+// Coding Challenge #1
 function BMI(weight, height) {
-  return weight / height ** 2;
+    return weight / height ** 2;
 }
 
 var marksWeight = 78;
@@ -14,3 +15,14 @@ var johnsBMI = BMI(johnsWeight, johnsHeight);
 const markHigherBMI = marksBMI > johnsBMI;
 
 console.log(marksBMI, johnsBMI, markHigherBMI);
+
+// Coding Challenge #2
+if (marksBMI === johnsBMI) {
+    console.log(
+        `Mark's BMI (${marksBMI}) is higher than John's (${johnsBMI})!`
+    );
+} else {
+    console.log(
+        `John's BMI (${johnsBMI}) is higher than Mark's (${marksBMI})!`
+    );
+}
